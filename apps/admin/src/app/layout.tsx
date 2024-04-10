cbbae6bb15890888a49999d14cc9c02b22f6360f
gaoooon <gaon9749@gmail.com>
2024-04-10 13:13:51 +0900

    Add next

---
+++ apps/admin/src/app/layout.tsx
@@ -0,0 +1,11 @@
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='ko'>
+      <body>{children}</body>
+    </html>
+  );
+}
