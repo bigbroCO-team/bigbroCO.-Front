36c33f50e79c86d7d3562db41832e4eef703069b
gaoooon <gaon9749@gmail.com>
2024-05-21 14:14:45 +0900

    Add Provider

---
+++ packages/share-components/src/components/Provider/index.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from '@emotion/react';
+
+import { theme } from '@/styles';
+
+interface Props {
+  readonly children: React.ReactNode;
+}
+
+const Provider: React.FC<Props> = ({ children }) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
+
+export default Provider;
