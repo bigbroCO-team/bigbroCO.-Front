268ecb58b403504fe73640ddce800023032834ec
gaoooon <gaon9749@gmail.com>
2024-06-05 21:59:09 +0900

    Update layout.tsx

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Provider } from 'share-components';
 
 const RootLayout = ({
@@ -6,6 +8,14 @@ const RootLayout = ({
   children: React.ReactNode;
 }>) => (
   <html lang='ko'>
+    <head>
+      <link
+        rel='stylesheet'
+        as='style'
+        crossOrigin=''
+        href='https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/variable/pretendardvariable-dynamic-subset.css'
+      />
+    </head>
     <body>
       <Provider>{children}</Provider>
     </body>
