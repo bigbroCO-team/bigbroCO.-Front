809748ea2fae02517c7df26179da34213ea9bd45
gjaegyun <jaegyungim06@gmail.com>
2024-06-10 18:15:48 +0900

    Add index.stories.tsx

---
+++ apps/client/src/components/ProductItem/index.stories.tsx
@@ -0,0 +1,18 @@
+import ProductItem from '.';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof ProductItem> = {
+  component: ProductItem,
+  args: {
+    productImg: 'name',
+    discountPercent: '20',
+    price: '25000',
+    productName: 'Big Bro T-shirt',
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ProductItem>;
+
+export const Primary: Story = {};
