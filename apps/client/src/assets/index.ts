37ffb62b224ed2000f396b74d97bbb20edd4a269
gjaegyun <jaegyungim06@gmail.com>
2024-06-12 23:41:00 +0900

    Add XIcon

--- apps/client/src/assets/index.ts
+++ apps/client/src/assets/index.ts
@@ -1,1 +1,5 @@
 export * from './eyesIcon';
+export { default as CheckBox } from './CheckBox';
+export { default as MinusIcon } from './MinusIcon';
+export { default as PlusIcon } from './PlusIcon';
+export { default as XIcon } from './XIcon';
