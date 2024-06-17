c2fb71f542452e7e4fcd542c4c06183cd653cd16
gjaegyun <jaegyungim06@gmail.com>
2024-06-17 17:36:10 +0900

    Add index.stories.tsx

---
+++ apps/client/src/components/SelectBtn/index.stories.tsx
@@ -0,0 +1,13 @@
+import SelectBtn from '.';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof SelectBtn> = {
+  component: SelectBtn,
+  args: {},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof SelectBtn>;
+
+export const Primary: Story = {};
