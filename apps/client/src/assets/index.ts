6ad435f156c50a25fbbb4fe57c192ab3debde511
gaoooon <gaon9749@gmail.com>
2024-06-25 21:12:54 +0900

    Add SCULFEE logo

--- apps/client/src/assets/index.ts
+++ apps/client/src/assets/index.ts
@@ -1,3 +1,5 @@
 export * from './eyesIcon';
 export { default as CBWASLogo } from './CBWASLogo';
 export { default as SCBLogo } from './SCBLogo';
+export { default as SCULFEELogo } from './SCULFEELogo';
+export { default as BIGBROLogo } from './BIGBROLogo';
