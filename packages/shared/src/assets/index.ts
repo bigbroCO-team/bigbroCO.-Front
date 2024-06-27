12d639e86319b43620da5d86222e67f70585e6a3
gaoooon <gaon9749@gmail.com>
2024-06-27 23:27:32 +0900

    Add GONGNEWGILogo

--- packages/shared/src/assets/index.ts
+++ packages/shared/src/assets/index.ts
@@ -3,3 +3,4 @@ export { default as BIGBROLogo } from './BIGBROLogo';
 export { default as CBWASLogo } from './CBWASLogo';
 export { default as SCBLogo } from './SCBLogo';
 export { default as SCULFEELogo } from './SCULFEELogo';
+export { default as GONGNEWGILogo } from './GONGNEWGILogo';
