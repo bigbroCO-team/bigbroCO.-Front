5ace37d61e7dad9e77add7189757b18cc5442e7c
gaoooon <gaon9749@gmail.com>
2024-06-29 20:57:20 +0900

    Update Header component

--- packages/shared/src/components/Header/index.tsx
+++ packages/shared/src/components/Header/index.tsx
@@ -1,40 +1,61 @@
-import { useRouter, useSelectedLayoutSegment } from 'next/navigation';
-import * as S from './style';
-import { useState } from 'react';
-import { BIGBROCOMPANYLogo } from 'shared/assets';
+'use client';
 
-const Header = () => {
-  const router = useRouter();
+import * as S from './style';
+import * as I from 'shared/assets';
+import Link from 'next/link';
+import { HeaderType } from 'shared/types';
+import { useSelectedLayoutSegment } from 'next/navigation';
+
+interface Props {
+  type: HeaderType;
+}
+
+const Header: React.FC<Props> = ({ type }) => {
   const segment = useSelectedLayoutSegment();
-  const navigator = (props: string) => {
-    router.push(props);
-  };
-  const [scrollValue, setScrollValue] = useState(0);
-  window.addEventListener('scroll', () => {
-    setScrollValue(window.scrollY);
-  });
-  const clientNavigator = ['CBWAS', 'S.C.B', 'BIGBRO', 'GONGNEWGI', 'SCULFEE'];
+
+  console.log(segment);
 
   return (
-    <S.Container type={scrollValue} path={segment}>
-      <S.LogoWapper href='/'>
-        <BIGBROCOMPANYLogo />
-      </S.LogoWapper>
-      <S.NavWrapper>
-        {clientNavigator.map((client, idx) => (
-          <S.Nav
-            key={idx}
-            type={client}
-            path={segment}
-            onClick={() => navigator(`/${client}`)}
-          >
-            {client}
-          </S.Nav>
-        ))}
-      </S.NavWrapper>
-      <S.Nav type={'Login'} path={segment} onClick={() => navigator(`/login`)}>
-        Login
-      </S.Nav>
+    <S.Container type={type} segment={segment}>
+      <Link href='/'>
+        <I.BIGBROCOMPANYLogo />
+      </Link>
+      <S.NavContainer>
+        {type !== 'admin' && (
+          <>
+            <Link href='/CBWAS'>
+              <I.CBWASLogo />
+            </Link>
+            <Link href='/S.C.B'>
+              <I.SCBLogo />
+            </Link>
+            <Link href='/BIGBRO'>
+              <I.BIGBROLogo />
+            </Link>
+            <Link href='/GONGNEWGI'>
+              <I.GONGNEWGILogo />
+            </Link>
+            <Link href='/SCULFEE'>
+              <I.SCULFEELogo />
+            </Link>
+          </>
+        )}
+        {type === 'admin' && (
+          <>
+            <S.AdminNav href='/'>상품 관리</S.AdminNav>
+            <S.AdminNav href='/'>주문 내역</S.AdminNav>
+          </>
+        )}
+      </S.NavContainer>
+      <S.LoginButton href='/login'>
+        <I.LoginText />
+      </S.LoginButton>
+      {/* <S.LogoutButton href='/logout'>
+        <I.LogoutText />
+      </S.LogoutButton>
+      <S.MyPageButton href='/'>
+        <I.MyPageText />
+      </S.MyPageButton> */}
     </S.Container>
   );
 };
--- packages/shared/src/components/Header/style.ts
+++ packages/shared/src/components/Header/style.ts
@@ -1,31 +1,105 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import Link from 'next/link';
+import { HeaderType } from 'shared/types';
 
-export const Container = styled.header<{ type: number; path: string | null }>`
+export const Container = styled.header<{
+  type: HeaderType;
+  segment: string | null;
+}>`
+  width: 100vw;
+  height: 4.8125rem;
+  padding: 0 7.5rem;
   display: flex;
-  position: fixed;
-  width: 100%;
-  padding: 1.5rem 0;
-  justify-content: space-around;
-  align-items: flex-end;
-  background: ${({ theme, type, path }) =>
-    path !== null && type > window.innerHeight ? theme.color.black : null};
-`;
-
-export const NavWrapper = styled.div`
-  display: flex;
-  width: 50rem;
+  align-items: center;
   justify-content: space-between;
-`;
 
-export const Nav = styled.nav<{ type: string; path: string | null }>`
-  color: ${({ theme, type, path }) => {
-    if (type === 'logo') {
-      return theme.color.white;
-    }
-    return path === type ? theme.color.main[50] : theme.color.grey[500];
+  // type에 맞는 컬려 변경 로직
+  ${({ theme, type }) =>
+    type === 'client_white'
+      ? css`
+          .BIGBRO,
+          .CBWAS,
+          .GONGNEWGI,
+          .SCB,
+          .SCULFEE,
+          .Text {
+            fill: ${theme.color.grey[600]};
+          }
+
+          .BIGBROCOMPANY {
+            fill: ${theme.color.black};
+          }
+        `
+      : css`
+          .BIGBRO,
+          .CBWAS,
+          .GONGNEWGI,
+          .SCB,
+          .SCULFEE,
+          .Text {
+            fill: ${theme.color.grey[500]};
+          }
+
+          .BIGBROCOMPANY {
+            fill: ${theme.color.white};
+          }
+        `}
+
+  ${({ theme, segment }) => {
+    if (segment === 'CBWAS')
+      return css`
+        .CBWAS {
+          fill: ${theme.color.main[50]} !important;
+        }
+      `;
+    if (segment === 'S.C.B')
+      return css`
+        .SCB {
+          fill: ${theme.color.main[50]} !important;
+        }
+      `;
+    if (segment === 'BIGBRO')
+      return css`
+        .BIGBRO {
+          fill: ${theme.color.main[50]} !important;
+        }
+      `;
+    if (segment === 'GONGNEWGI')
+      return css`
+        .GONGNEWGI {
+          fill: ${theme.color.main[50]} !important;
+        }
+      `;
+    if (segment === 'SCULFEE')
+      return css`
+        .SCULFEE {
+          fill: ${theme.color.main[50]} !important;
+        }
+      `;
   }};
-  ${({ theme }) => theme.typo.titleSmall};
 `;
 
-export const LogoWapper = styled(Link)``;
+export const NavContainer = styled.div`
+  display: flex;
+  gap: 3.75rem;
+`;
+
+export const AdminNav = styled(Link)`
+  // theme에 정의 되어 있지 않아서 임시로 지정
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  color: ${({ theme }) => theme.color.grey[500]};
+`;
+
+export const LoginButton = styled(Link)`
+  margin-left: 1.1875rem;
+`;
+
+export const LogoutButton = styled(Link)`
+  margin-left: 0.3125rem;
+`;
+
+export const MyPageButton = styled(Link)``;
