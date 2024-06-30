83b4067e29fab626ea0a512f13048154b71be9a0
gaoooon <gaon9749@gmail.com>
2024-06-30 22:53:57 +0900

    Update index.stories.tsx

--- packages/shared/src/components/Header/index.stories.tsx
+++ packages/shared/src/components/Header/index.stories.tsx
@@ -3,6 +3,7 @@ import Header from '.';
 
 const meta: Meta<typeof Header> = {
   component: Header,
+  args: { type: 'client_black' },
 };
 
 export default meta;
@@ -10,3 +11,27 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+
+export const ClientBlack: Story = {
+  args: {
+    type: 'client_black',
+  },
+};
+
+export const ClientWhite: Story = {
+  args: {
+    type: 'client_white',
+  },
+};
+
+export const ClientImg: Story = {
+  args: {
+    type: 'client_img',
+  },
+};
+
+export const Admin: Story = {
+  args: {
+    type: 'admin',
+  },
+};
